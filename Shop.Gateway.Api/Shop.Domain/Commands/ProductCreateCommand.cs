7286a4241dc812328a217f73d007d91c521e3356
Melnikov Maxim <melnikovmaxim@icloud.com>
2022-06-09 23:40:29 +0300

    Создал команду создания продукта и событие создания продукта

---
+++ Shop.Gateway.Api/Shop.Domain/Commands/ProductCreateCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Domain.Commands;
+
+public class ProductCreateCommand
+{
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = null!;
+    public string ProductDescription { get; set; } = null!;
+    public decimal ProductPrice { get; set; }
+    public Guid CategoryId { get; set; }
+}
---
+++ Shop.Gateway.Api/Shop.Domain/Events/ProductCreatedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Shop.Domain.Events;
+
+public class ProductCreatedEvent
+{
+    public Guid ProductId { get; set; }
+    public string ProductName { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+}
