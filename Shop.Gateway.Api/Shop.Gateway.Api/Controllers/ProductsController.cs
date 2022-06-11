66e7315d4faf90c4f1c55b9449e522b9616f110a
Melnikov Maxim <melnikovmaxim@icloud.com>
2022-06-11 14:39:38 +0300

    Контроллер продуктов в шлюз

---
+++ Shop.Gateway.Api/Shop.Gateway.Api/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Domain.Commands;
+
+namespace Shop.Gateway.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ProductsController: ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    public IActionResult GetProduct() => Accepted();
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    public IActionResult AddProduct([FromBody] ProductCreateCommand command) => Accepted();
+}
