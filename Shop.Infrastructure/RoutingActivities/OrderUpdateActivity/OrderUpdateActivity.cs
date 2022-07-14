9520714ce04bca5510e36de03710e3ff1779e667
Максим Мельников <maxim.melnikov@globalports.com>
2022-07-14 18:05:09 +0300

    Начало реализации распределенных транзакций

---
+++ Shop.Infrastructure/RoutingActivities/OrderUpdateActivity/OrderUpdateActivity.cs
@@ -0,0 +1,39 @@
+﻿using MassTransit;
+
+namespace Shop.Infrastructure.RoutingActivities.UpdateOrderActivity;
+
+using Order;
+
+public class OrderUpdateActivity: IActivity<Order, OrderUpdateLog>
+{
+    public async Task<ExecutionResult> Execute(ExecuteContext<Order> context)
+    {
+        try
+        {
+            var endpoint = await context.GetSendEndpoint(new Uri("rabbitmq://localhost/create_order"));
+
+            await endpoint.Send(context.Arguments);
+
+            var log = new OrderUpdateLog()
+            {
+                OrderId = context.Arguments.OrderId
+            };
+            
+            return context.Completed(log);
+        }
+        catch(Exception _)
+        {
+            return context.Faulted();
+        }
+    }
+
+    public Task<CompensationResult> Compensate(CompensateContext<OrderUpdateLog> context)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class OrderUpdateLog
+{
+    public string OrderId { get; set; } = null!;
+}
---
+++ Shop.Order.Api/Consumers/OrderPlaceConsumer.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using MassTransit.Courier.Contracts;
+
+namespace Shop.Order.Api.Consumers;
+
+using Infrastructure.Order;
+
+public class OrderPlaceConsumer: IConsumer<Order>
+{
+    public Task Consume(ConsumeContext<Order> context)
+    {
+        throw new NotImplementedException();
+    }
+
+    private RoutingSlip CreateRoutingSlip(Order order, ConsumeContext<Order> context)
+    {
+        var builder = new RoutingSlipBuilder(new Guid(order.OrderId));
+    }
+}
